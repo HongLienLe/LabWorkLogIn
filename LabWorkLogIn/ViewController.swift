cebfc6e176750e9571535a45f25032f4b849a245
Hong Le <Hongle050@gmail.com>
2018-11-28 16:20:55 +0000

    Lab work Log in, used downcasting, segues, navigation controls

--- LabWorkLogIn/ViewController.swift
+++ LabWorkLogIn/ViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var forgottenUserNameButton: UIButton!
+    @IBOutlet weak var forgottenPasswordButton: UIButton!
+    @IBOutlet weak var UserNameTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
 
+        guard let sender = sender as? UIButton else {return}
 
+        if sender == forgottenPasswordButton {
+            segue.destination.navigationItem.title = "Forgot Password"
+        } else if sender == forgottenUserNameButton {    segue.destination.navigationItem.title = "Forgot Username"
+        } else {
+            segue.destination.navigationItem.title =
+                UserNameTextField.text
+        }
+          
+
+    }
+    @IBAction func forgottenUserNamePressed(_ sender: Any) {
+performSegue(withIdentifier: "view", sender: forgottenUserNameButton)
+        
+        
+        
+    }
+    @IBAction func forgottenPasswordPressed(_ sender: Any) {
+        performSegue(withIdentifier: "view", sender: forgottenPasswordButton)
+    }
+    
 }
 
